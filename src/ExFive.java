c4425cef4992f3ea9186cf8de8474226328d04da
manoj <manoj.sarwate@ssiedu.in>
2019-06-07 12:20:15 +0530

    07-06-19

---
+++ src/ExFive.java
@@ -0,0 +1,14 @@
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ExFive {
+
+    public static void f1() throws ClassNotFoundException{
+        
+        
+            Class.forName("abc");
+        
+    }
+    
+}
---
+++ src/ExSix.java
@@ -0,0 +1,7 @@
+public class ExSix {
+    public void f1(){}
+            
+    public static void f2() throws ClassNotFoundException{
+        ExFive.f1();
+    }
+}
